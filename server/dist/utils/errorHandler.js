de0082083c94d4c9de5835d275af06d9ad3f6e13
Varsha Dewangan <140236223+Varsha-1605@users.noreply.github.com>
2024-08-16 14:10:32 +0530

    Create errorHandler.js

---
+++ server/dist/utils/errorHandler.js
@@ -0,0 +1,7 @@
+"use strict";
+// import { Request, Response, NextFunction } from 'express';
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+};
+module.exports = errorHandler;
