94c964f8377d683b1b286fa85e8dc4179ed342c6
Varsha Dewangan <140236223+Varsha-1605@users.noreply.github.com>
2024-08-16 17:06:21 +0530

    Update chatRoutes.ts

--- server/src/routes/chatRoutes.ts
+++ server/src/routes/chatRoutes.ts
@@ -34,7 +34,6 @@ import express from 'express';
 import { sendMessage } from '../controllers/chatController';
 
 const router = express.Router();
-
 router.post('/', sendMessage);
 
-export = router;
+export default router;
